d186b42862637761aa11a8a67d717392def72612
Krigers <kristersgredzens@gmail.com>
2025-04-16 23:54:03 +0300

    instantiate objetcs

--- classes/Car.php
+++ classes/Car.php
@@ -1,12 +1,17 @@
 <?php
 
 class Car {
-    private $mobile;
-    private $car;
-    private $raketa = "boom";
+    private $gpu;
+    private $cpu;
+    public $raketa = "boom";
 
-    public function __construct($mobile, $car) {
-        $this->mobie = $mobile;
-        $this->car = $car;
+    public function __construct($gpu = "poopoo", $cpu) {
+        $this->gpu = $gpu;
+        $this->cpu = $cpu;
     }
 }
+
+$gpu01 = new gpu("GTX", "1060");
+$gpu02 = new mobile("RTX", "3060");
+$gpu02 = new mobile("GT");
+
