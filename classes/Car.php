816c9857d1889045e91f5a7893500b6e5e2064d3
Krigers <kristersgredzens@gmail.com>
2025-04-17 00:38:05 +0300

    new input output

--- classes/Car.php
+++ classes/Car.php
@@ -1,17 +1,25 @@
 <?php
 
-class Car {
-    private $gpu;
-    private $cpu;
-    public $raketa = "boom";
+class gpu {
+    public $gpu;
+    public $form;
+    public $raketa = "boobm";
 
-    public function __construct($gpu = "poopoo", $cpu) {
+    public function __construct($form, $gpu = "poopoo") {
         $this->gpu = $gpu;
-        $this->cpu = $cpu;
+        $this->form = $form;
+
+        //Method
     }
-}
+    public function getGpuInfo() {
+        return "Gpu: " . $this->gpu . "; Form: " . $this->form;
+        }
+};
 
 $gpu01 = new gpu("GTX", "1060");
-$gpu02 = new mobile("RTX", "3060");
-$gpu02 = new mobile("GT");
+$gpu02 = new gpu("RTX", "3060");
+$gpu03 = new gpu("GT");
 
+echo $gpu01->raketa;
+echo "<br>";
+echo $gpu01 -> getGpuInfo();
