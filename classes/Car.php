28d914c459a70ecd0b62e8985a58f3b774793e86
Krigers <kristersgredzens@gmail.com>
2025-04-16 23:44:42 +0300

    .\classes\

---
+++ classes/Car.php
@@ -0,0 +1,12 @@
+<?php
+
+class Car {
+    private $mobile;
+    private $car;
+    private $raketa = "boom";
+
+    public function __construct($mobile, $car) {
+        $this->mobie = $mobile;
+        $this->car = $car;
+    }
+}
